2fd130fe621d896e4867026a51a22d6deb0890b7
Arshadul Hossain <Asadulit@gmail.com>
2025-09-23 04:27:52 +0600

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -85,6 +85,48 @@ const Contact = () => {
     '$5,000+/month'
   ];
 
+  export default function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "96c09ea0-464f-408b-a98a-e4db232e359b");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" required/>
+        <input type="email" name="email" required/>
+        <textarea name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+      </form>
+      <span>{result}</span>
+
+    </div>
+  );
+}
+
 return (
     <section id="contact" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
